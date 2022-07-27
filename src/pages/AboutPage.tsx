8f896ff2399d14cf4241a49a001cda469f8ca639
Adrian Morari <adrian.morari.mih@gmail.com>
2022-07-27 16:44:42 +0300

    first commit

---
+++ src/pages/AboutPage.tsx
@@ -0,0 +1,7 @@
+export function AboutPage() {
+    return (
+        <div>
+            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quis odio officiis illo repellat reiciendis reprehenderit soluta dolores earum eum deleniti voluptatem alias, aut debitis laboriosam numquam, quibusdam, commodi quidem. Hic?
+        </div>
+    )
+}
